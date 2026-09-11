ef7ae45e04c85d8cefcbf0a8033e6e275fc0c0c6
Vatika <saxenavati@gmail.com>
2026-09-11 13:54:33 +0000

    Add Terraform CI/CD workflow

---
+++ terraform-cicd/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "cicd_demo" {
+  bucket = "vatika-terraform-cicd-demo-2026"
+}
