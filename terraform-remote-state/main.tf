dc9b047e2a3ec06e939fa18a3699d7234f38b09a
Ishu <your-email@example.com>
2026-09-14 14:13:37 +0000

    Add Terraform remote S3 backend configuration

---
+++ terraform-remote-state/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "s3" {
+    bucket       = "vatika-terraform-state-2026"
+    key          = "terraform-remote-state/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "vatika-terraform-backend-demo-2026"
+}
